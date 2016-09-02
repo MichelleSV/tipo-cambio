e242dd211e6c7cf28a2d7272f602dfce7a4f44e8
aifos710 <sofia.m.caballero@gmail.com>
2016-09-02 16:14:41 -0500

    modificando js

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function() {
 		var soles = parseInt(document.getElementById("soles").value);
 
 		var resultado = document.getElementById("resultado");
-		resultado.innerHTML = "<p>Tu monto es " + cambio(tipoDeCambio,soles).toFixed(2) + "</p>";
+		resultado.innerHTML = "<p>Tu monto es $" + cambio(tipoDeCambio,soles).toFixed(2) + "</p>";
 	});
 
 	function cambio(tipoDeCambio,soles) {
