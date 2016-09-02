02bda14122008a10407a00b317b3c6da68161007
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-09-02 15:39:39 -0500

    añadiendo js, html y css

---
+++ js/app.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function() {
+	var boton = document.getElementById("calcular");
+	boton.addEventListener("click", function() {
+		var tipoDeCambio = parseInt(document.getElementById("tipoDeCambio").value);
+		var soles = parseInt(document.getElementById("soles").value);
+
+		var resultado = document.getElementById("resultado");
+		resultado.innerHTML = "<p>Tu monto es " + cambio(tipoDeCambio,soles).toFixed(2) + "</p>";
+	});
+
+	function cambio(tipoDeCambio,soles) {
+		return soles/ tipoDeCambio;
+	}
+});
