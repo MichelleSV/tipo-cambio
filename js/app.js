7a3b917845976d4ba31de3e730722e3b3c8b580a
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-09-02 17:26:39 -0500

    validando si no hay ningun numero

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 window.addEventListener("load", function() {
-	var boton = document.getElementById("calcular");
-	boton.addEventListener("click", function() {
-		var tipoDeCambio = parseFloat(document.getElementById("tipoDeCambio").value);
-		var soles = parseFloat(document.getElementById("soles").value);
-
-		var resultado = document.getElementById("resultado");
-		resultado.innerHTML = "<p>Tu monto es $" + cambio (tipoDeCambio,soles) + "</p>";
-	});
-
-	function cambio(tipoDeCambio,soles) {
-		return (soles/ tipoDeCambio).toFixed(2);
-	}
+    var boton = document.getElementById("calcular");
+    boton.addEventListener("click", function() {
+        var tipoDeCambio = parseFloat(document.getElementById("tipoDeCambio").value);
+        var soles = parseFloat(document.getElementById("soles").value);
+        var resultado;
+        if(soles && tipoDeCambio){
+            resultado=  document.getElementById("resultado");
+            resultado.innerHTML = "<p>El resultado es " + cambio(tipoDeCambio,soles) + "</p>";
+        } else {
+            resultado= document.getElementById("resultado");
+            resultado.innerHTML = "<p>Ingrese un valor</p>";
+        }
+    });
+    function cambio(tipoDeCambio, soles) {
+        return (soles / tipoDeCambio).toFixed(2);
+    }
 });
