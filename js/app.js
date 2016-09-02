7658af92058ece1f2992ef56a1cfc8c5640d9073
aifos710 <sofia.m.caballero@gmail.com>
2016-09-02 16:20:42 -0500

    validando calculo

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 window.addEventListener("load", function() {
 	var boton = document.getElementById("calcular");
 	boton.addEventListener("click", function() {
-		var tipoDeCambio = parseInt(document.getElementById("tipoDeCambio").value);
-		var soles = parseInt(document.getElementById("soles").value);
+		var tipoDeCambio = parseFloat(document.getElementById("tipoDeCambio").value);
+		var soles = parseFloat(document.getElementById("soles").value);
 
 		var resultado = document.getElementById("resultado");
-		resultado.innerHTML = "<p>Tu monto es $" + cambio(tipoDeCambio,soles).toFixed(2) + "</p>";
+		resultado.innerHTML = "<p>Tu monto es $" + cambio (tipoDeCambio,soles) + "</p>";
 	});
 
 	function cambio(tipoDeCambio,soles) {
-		return soles/ tipoDeCambio;
+		return (soles/ tipoDeCambio).toFixed(2);
 	}
 });
